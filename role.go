11ad94dc084fea5f5854098986cbcca6d927b2be
kenqshen <kenqshen@tencent.com>
2022-04-29 12:53:42 +0800

    feat: complement ops for user, role

--- role.go
+++ role.go
@@ -1,11 +1,12 @@
 package src
 
-type Role struct{
+type Role struct {
+	name        string
 	permissions []*Permission
 }
 
-func NewRole(permissions []*Permission) *Role {
-	return &Role{permissions:permissions}
+func NewRole(name string, permissions []*Permission) *Role {
+	return &Role{name: name, permissions: permissions}
 }
 
 func (role *Role) permit(comp_permission *Permission) bool {
@@ -15,4 +16,28 @@ func (role *Role) permit(comp_permission *Permission) bool {
 		}
 	}
 	return false
+}
+
+func (role *Role) assign(new_permission *Permission) {
+	// check if exist
+	// todo optimize
+	for _, permission := range role.permissions {
+		if new_permission.name == permission.name {
+			return
+		}
+	}
+	role.permissions = append(role.permissions, new_permission)
+	return
+}
+
+func (role *Role) revoke(revoke_permission *Permission) {
+	// check if exist before delete
+	// todo optimize
+	for i := 0; i < len(role.permissions); i++ {
+		if revoke_permission.name == role.permissions[i].name {
+			role.permissions = append(role.permissions[:i], role.permissions[i+1:]...)
+			return
+		}
+	}
+	return
 }
--- user_test.go
+++ user_test.go
@@ -1,11 +1,34 @@
 package src
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestNewUser(t *testing.T) {
 	p0 := NewPermission("p0")
 	p1 := NewPermission("p1")
-	ps := []*Permission{p0, p1}
-	r0 := NewRole(ps)
-	u0 := NewUser(1)
+	ps0 := []*Permission{p0}
+	ps1 := []*Permission{p1}
+	r0 := NewRole("r0", ps0)
+	r1 := NewRole("r1", ps1)
+	rs := []*Role{r0, r1}
+	// c
+	u1 := NewUser(1, "u1", nil)
+	// r
+	fmt.Println("c u1: ", u1, u1.permit(p1))
+	for _, role := range u1.roles {
+		fmt.Println("r u1 role ", role.name)
+	}
+	// u d
+	u1.grant(rs)
+	for _, role := range u1.roles {
+		fmt.Println("r u u1 role ", role.name)
+	}
+	r2 := NewRole("r0", ps0)
+	u1.revoke(r2)
+	for _, role := range u1.roles {
+		fmt.Println("r d u1 role ", role.name)
+	}
+	fmt.Println("u0: ", u1, u1.permit(p1))
 }
