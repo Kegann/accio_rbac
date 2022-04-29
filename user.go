3ebe491643ebcb9f4444629ac4def0e4f701eb9e
kenqshen <kenqshen@tencent.com>
2022-04-29 12:53:04 +0800

    feat: complement ops for user, role

--- user.go
+++ user.go
@@ -1,27 +1,38 @@
 package src
 
 type User struct {
-	id int64
+	id    int64
+	name  string
 	roles []*Role
 }
 
-func NewUser (id int64) *User {
-	return &User{id:id}
+func NewUser(id int64, name string, roles []*Role) *User {
+	return &User{id: id, name: name, roles: roles}
 }
 
 /*func (user *User) loads () error {
 }*/
 
-func (user *User) grant (roles []*Role) {
+func (user *User) grant(roles []*Role) {
 	user.roles = roles
 	return
 }
 
-func (user *User) permit (permission *Permission) bool {
+func (user *User) permit(permission *Permission) bool {
 	for _, role := range user.roles {
 		if ok := role.permit(permission); ok {
 			return true
 		}
 	}
 	return false
+}
+
+func (user *User) revoke(revoke_role *Role) {
+	for i := 0; i < len(user.roles); i++ {
+		if revoke_role.name == user.roles[i].name {
+			user.roles = append(user.roles[:i], user.roles[i+1:]...)
+			return
+		}
+	}
+	return
 }
